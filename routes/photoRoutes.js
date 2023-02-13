437edef34173cbcabe4efb7342b494dc83f425ff
ronnenn podolsky <podolsky.ronnen@gmail.com>
2023-02-13 16:54:31 +0200

    Part III - Advanced Routes: Return Combined Data

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -13,6 +13,28 @@ const unsplash = axios.create({
 });
 
 
+router.get('/user/:username', async (req, res) => {
+    try {
+        const { username } = req.params
+        const response = await unsplash
+            .get(`/users/${username}/photos/`)
+        const data = await response.data;
+        let userPhotos = data.map(photo => {
+            return {
+                id: photo.id,
+                user: photo.user.username,
+                description: photo.description ? photo.description : "No description provided.",
+                url: photo.urls.raw
+            }
+        })
+        res.status(200).json(userPhotos);
+    }
+    catch (error) {
+        console.log(error.response);
+        res.status(error.response.status).json({ message: error.response.statusText })
+    }
+})
+
 router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params
@@ -34,13 +56,15 @@ router.get('/', async (req, res) => {
             .get('photos/')
         const data = await response.data;
         const raw_urls = data.map(v => v.urls.raw)
-        res.status(200).json(data);
+        res.status(200).json(raw_urls);
     }
     catch (e) {
         console.log(e.message)
         res.status(500).json({ message: 'Server error. Please try again later.' })
     }
 })
+
+
 
 
 export { router as photosRouter };
