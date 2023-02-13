4b7b064c6981288420675abc73cd5b84d9977d6d
ronnenn podolsky <podolsky.ronnen@gmail.com>
2023-02-13 20:05:11 +0200

    removed axios instance from router

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -4,16 +4,6 @@ const router = express.Router();
 
 import { getPhoto, getPhotos, getUserPhotos } from '../controllers/photoController.js';
 
-
-// router.route('/').get(protect, getGoals).post(protect, setGoals)
-// router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
-
-const unsplash = axios.create({
-    baseURL: 'https://api.unsplash.com/',
-    headers: { 'Authorization': `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}` }
-});
-
-
 router.get('/user/:username', getUserPhotos)
 router.get('/:id', getPhoto)
 router.get('/', getPhotos)
