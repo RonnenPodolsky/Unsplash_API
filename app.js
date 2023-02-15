a809e66115428c6abca5d902b077f5efedfe4af2
ronnenn podolsky <podolsky.ronnen@gmail.com>
2023-02-15 03:10:47 +0200

    really last refactor

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-
 import dotenv from 'dotenv';
 import express from "express";
 import { connectDB } from './config/db.js';
+import { protect } from './middleware/authMiddleware.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import { favoriteRouter } from "./routes/favoriteRoutes.js";
 import { photosRouter } from "./routes/photoRoutes.js";
 import { usersRouter } from "./routes/userRoutes.js";
+
 dotenv.config()
 connectDB()
 
@@ -15,13 +16,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Unsplash API!' })
+})
+
 app.use('/api/photos', photosRouter)
 app.use('/api/users', usersRouter)
-app.use('/api/favorites', favoriteRouter)
-app.use(errorHandler)
+app.use('/api/favorites', protect, favoriteRouter)
 
-app.use('/', (req, res) => {
-    res.status(200).json({ 'message': 'Welcome to the Unsplash API!' })
-})
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "Resource not found" });
+});
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { addPhoto, editPhotoDesc, getAllPhotosByUser, removeFavPhoto } from '../controllers/favoritesController.js';
-import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/add', protect, addPhoto)
-router.route('/:id').delete(protect, removeFavPhoto).put(protect, editPhotoDesc)
-router.get('/', protect, getAllPhotosByUser)
+router.post('/add', addPhoto)
+router.route('/:id').delete(removeFavPhoto).put(editPhotoDesc)
+router.get('/', getAllPhotosByUser)
 
 export { router as favoriteRouter };
